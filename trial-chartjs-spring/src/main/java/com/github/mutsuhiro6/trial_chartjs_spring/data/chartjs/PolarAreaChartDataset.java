c519919c821e0a3062aa8c737d18bbf46a842eba
Mutsuhiro Iwamoto <mutsuhiro6@icloud.com>
2021-07-17 01:48:55 +0900

    add basic chart properties

--- trial-chartjs-spring/src/main/java/com/github/mutsuhiro6/trial_chartjs_spring/data/chartjs/PolarAreaChartDataset.java
+++ trial-chartjs-spring/src/main/java/com/github/mutsuhiro6/trial_chartjs_spring/data/chartjs/PolarAreaChartDataset.java
@@ -1,11 +1,13 @@
 package com.github.mutsuhiro6.trial_chartjs_spring.data.chartjs;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class PolarAreaChartDataset extends Dataset<Double> {
 
   private Color backgroundColor;
